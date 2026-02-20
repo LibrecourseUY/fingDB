f57b9d4cc875a2185554fb109bdfbf83f70b74f0
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-02-20 02:59:01 -0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ dotenv_path = os.path.join(PROJECT_ROOT, ".env")
 load_dotenv(dotenv_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+ADMIN_USER = os.getenv("ADMIN_USER")
 ADMIN_PSWD = os.getenv("ADMIN_PSWD")
 SECRET_KEY = os.getenv("SECRET_KEY")
 API_KEY = os.getenv("API_KEY")
